4050bfb1e6702125444607b84ecc00c657172095
julianxrenner <julesman18@gmail.com>
2025-06-11 15:23:38 -0400

    First Commit

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import Search from "./components/Search.jsx";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity_desc`
+      const response = await fetch()
+    } catch (error) {
+      console.log(`There was a problem fetching movies ${error}`);
+    }
+  };
+
+  useEffect(() => {}, []);
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You Will Enjoy
+            Without The Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default App;
